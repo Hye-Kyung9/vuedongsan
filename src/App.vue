b72bd40d2407edadb458415f5ff32c868bb40f45
Hye-Kyung9 <rngprud123@naver.com>
2022-07-29 17:18:22 +0900

    Vue EventHandler 연습

--- src/App.vue
+++ src/App.vue
@@ -3,25 +3,45 @@
     <a v-for="작명 in 메뉴들" :key="작명">{{작명}}</a>
   </div>
   <div>
-    <h4 :style="스타일">
-      <ul>
-      <li v-for="product in products" :key="product">{{product}}</li>
-      <li v-for="(price,i) in price" :key="i" >{{price}}만원</li>
-      </ul>
-    </h4>
+    <!-- <div v-for="(a,i) in products" :key="i">
+      <h4>{{products[i]}}</h4>
+      <p>50만원</p>
+    </div> -->
+    <div>
+      <h4>{{products[0]}}</h4>
+      <p>50만원</p>
+      <button @click="신고수[0]++">허위매물신고</button>
+      <span>신고수 : {{신고수[0]}}</span>
+    </div>
+    <div>
+      <h4>{{products[1]}}</h4>
+      <p>60만원</p>
+      <button @click="신고수[1]++">허위매물신고</button>
+      <span>신고수 : {{신고수[1]}}</span>
+    </div>
+    <div>
+      <h4>{{products[2]}}</h4>
+      <p>70만원</p>
+      <button @click="신고수[2]++">허위매물신고</button>
+      <span>신고수 : {{신고수[2]}}</span>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: 'App',
   data(){
     return {
-      price: [50,30,60],
+      신고수 : [0,0,0],
       메뉴들: ['HOME','PRODUCTS','ABOUT'],
       products :  ['역삼동원룸', '천호동원룸', '마포구원룸'],
     }
+  },
+  methods:{
+    // increase(){
+    //   this.신고수 ++;
+    // }
   },
   components: {
   }
@@ -47,7 +67,7 @@ export default {
   padding : 10px;
 }
 li, ol, ul {
-    list-style: none;
+  list-style: none;
 }
 </style>
  
