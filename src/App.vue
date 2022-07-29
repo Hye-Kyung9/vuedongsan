a942db5a1c21bd7648176269d254d5c60739ef2c
Hye-Kyung9 <rngprud123@naver.com>
2022-07-29 16:38:54 +0900

    Vue data binding, v-for 문법

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="menu">
+    <a v-for="작명 in 메뉴들" :key="작명">{{작명}}</a>
+  </div>
+  <div>
+    <h4 :style="스타일">
+      <ul>
+      <li v-for="product in products" :key="product">{{product}}</li>
+      <li v-for="(price,i) in price" :key="i" >{{price}}만원</li>
+      </ul>
+    </h4>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      price: [50,30,60],
+      메뉴들: ['HOME','PRODUCTS','ABOUT'],
+      products :  ['역삼동원룸', '천호동원룸', '마포구원룸'],
+    }
+  },
   components: {
-    HelloWorld
   }
 }
 </script>
@@ -23,4 +37,17 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+.menu {
+  background : darkslateblue;
+  padding : 15px;
+  border-radius : 5px;
+}
+.menu a {
+  color : white;
+  padding : 10px;
+}
+li, ol, ul {
+    list-style: none;
+}
 </style>
+ 
